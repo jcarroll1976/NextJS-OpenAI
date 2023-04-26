5824e1d322f1ecfb07b96aca5e1f0fa25b59c6a0
Joshua Carroll <carrolljosh1976@gmail.com>
2023-04-25 22:15:46 -0400

    Added post folder, new,[postId], and token-topup pages

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
   <div>
-    this is the homepage
+    <h1>this is the homepage</h1>
   </div>
   )
 }
---
+++ pages/token-topup.js
@@ -0,0 +1,7 @@
+export default function TokenTopup() {
+    return (
+    <div>
+      <h1>this is the token topup page</h1>
+    </div>
+    )
+  }
