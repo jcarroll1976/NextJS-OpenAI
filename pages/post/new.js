d02483a307d81d427bd51b900e53f2dc2295db41
Joshua Carroll <carrolljosh1976@gmail.com>
2023-04-26 18:04:16 -0400

    Added login authorization for project

--- pages/post/new.js
+++ pages/post/new.js
@@ -1,8 +1,17 @@
-export default function NewPost() {
+import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+
+export default function NewPost(props) {
+  console.log("NEW POST PROPS: ", props);
     return (
     <div>
       <h1>this is the new post page</h1>
     </div>
     )
   }
+
+  export const getServerSideProps = withPageAuthRequired(() => {
+    return {
+      props: {},
+    };
+  });
   
